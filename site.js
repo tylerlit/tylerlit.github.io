dea2e5c1eaa191d7ec06867fbf6211d72457cddd
Tyler Little <tyler10@umbc.edu>
2017-10-17 00:01:20 -0400

    Add files via upload

--- site.js
+++ site.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
 		turtle.happiness = Number(getCookie("happiness"));
 		turtle.size = getCookie("size");
 		var w = turtle.size + '%';
-		wnew = { width: w };
+		var wnew = { width: w };
 		if (turtle.happiness == '3'){
 			$("#turtle3").animate(wnew);
 		}
